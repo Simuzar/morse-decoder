3661d3c34b8f8b24c10f8bacf31bdf231d4cef29
Simuzar <fatulazade96@gmail.com>
2024-08-17 11:46:05 +0300

    docs: update the solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let wordsBinary = expr.match(/.{1,10}/g);
+    let lettersBinary = [];
+    let morseSigns = [];
+    let regWords = []
+
+    wordsBinary.forEach(i => {
+        if( i === '**********' ) {
+            lettersBinary.push(' ');
+        } else {
+            lettersBinary.push( i.match(/.{1,2}/g) );
+        }
+         } );
+
+    lettersBinary.forEach(i => {
+
+        if (Array.isArray(i)) {
+
+            morseSigns.push( i.map( char => char === '10' ? '.' : char === '11' ? '-' : '' ).join('') ); 
+
+        } else {
+             morseSigns.push('')
+        }
+        }
+    )
+
+    morseSigns.forEach(i => {
+        if(i === '') {
+            regWords.push(' ');
+        } else if ( typeof i === 'string') {
+            for( let key in MORSE_TABLE) {
+                if(key === i) {
+                    regWords.push(MORSE_TABLE[key] )
+                } 
+            } 
+        }
+    })
+   
+    // console.log(wordsBinary);
+    // console.log(lettersBinary);
+    // console.log(morseSigns);
+    return regWords.join('');
+    };
+
 
 module.exports = {
     decode
 }
+
